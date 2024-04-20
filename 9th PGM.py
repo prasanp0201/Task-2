f59481ee7961922e3d94e584f2ea9a9ac67b4545
prasanp0201 <167601879+prasanp0201@users.noreply.github.com>
2024-04-20 20:21:52 +0530

    Delete 9th PGM.py

--- 9th PGM.py
+++
@@ -1,6 +0,0 @@
-def count_words(input_string):
-    words_list = input_string.split()     # Split the input string into words based on whitespace
-    return len(words_list) # Return the length of the words list, which gives the count of words
-input_string = "Eva, can I see bees in a cave"
-word_count = count_words(input_string)
-print("Number of Words:", word_count)
