399bd10fc22da90ed02bcc36da8b1cfcec02c1be
prasanp0201 <167601879+prasanp0201@users.noreply.github.com>
2024-04-20 20:21:44 +0530

    Delete 8th PGM.py

--- 8th PGM.py
+++
@@ -1,9 +0,0 @@
-def is_anagram(str1, str2):
-    # Remove spaces and convert strings to lowercase
-    str1 = str1.replace(" ", "").lower()
-    str2 = str2.replace(" ", "").lower()
-    return sorted(str1) == sorted(str2)     # Check if the sorted versions of the strings are equal
-string1 = "typhon"
-string2 = "python"
-result = is_anagram(string1, string2)
-print("Are '{}' and '{}' anagrams?".format(string1, string2), result)
